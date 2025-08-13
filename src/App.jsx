7b2cdc7d8de1bac032024cf93de78751d152278c
xoxomakkie <makaylagunterm@hotmail.com>
2025-08-13 09:12:03 -0400

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SpacecraftBuilder from "./components/SpacecraftBuilder.jsx";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Spacecraft Builder 🛠️</h1>
+      <SpacecraftBuilder />
+    </div>
+  );
+}
+
+export default App;
